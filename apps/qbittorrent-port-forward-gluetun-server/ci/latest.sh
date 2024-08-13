940e49f4eee9235ca12dae50a247c05218a6f74a
Zilch25 <jsimcina@gmail.com>
2024-08-13 18:38:14 -0500

    i have no idea what i just did

--- apps/qbittorrent-port-forward-gluetun-server/ci/latest.sh
+++ apps/qbittorrent-port-forward-gluetun-server/ci/latest.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-git clone --quiet https://github.com/mjmeli/qbittorrent-port-forward-gluetun-server.git /tmp/qbittorrent-port-forward-gluetun-server
-sed -i '/\gtn_addr="/a mam_location="${MAM_ID_PATH}"' /tmp/qbittorrent-port-forward-gluetun-server/main.sh
-sed -i '/\echo "Successfully updated port"/i curl -c $mam_location -b $mam_location https://t.myanonamouse.net/json/dynamicSeedbox.php\n' /tmp/qbittorrent-port-forward-gluetun-server/main.sh
-pushd /tmp/qbittorrent-port-forward-gluetun-server > /dev/null || exit
-version=$(git rev-list --count --first-parent HEAD)
-popd > /dev/null || exit
-rm -rf /tmp/qbittorrent-port-forward-gluetun-server
-printf "1.0.%d" "${version}"
+version="$(curl -sX GET "https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
