fdfcf6075c39a03158a31609625f4c67bb023241
Zilch25 <jsimcina@gmail.com>
2024-08-13 18:20:58 -0500

    little path fix

--- apps/qbittorrent-port-forward-gluetun-server/ci/latest.sh
+++ apps/qbittorrent-port-forward-gluetun-server/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git clone --quiet https://github.com/mjmeli/qbittorrent-port-forward-gluetun-server.git /tmp/qbittorrent-port-forward-gluetun-server
-sed -i '/\gtn_addr="/a mam_location="${MAM_ID_PATH}"' ./apps/qbittorrent-port-forward-gluetun-server/main.sh
+sed -i '/\gtn_addr="/a mam_location="${MAM_ID_PATH}"' /tmp/qbittorrent-port-forward-gluetun-server/main.sh
 sed -i '/\echo "Successfully updated port"/i curl -c $mam_location -b $mam_location https://t.myanonamouse.net/json/dynamicSeedbox.php\n' /tmp/qbittorrent-port-forward-gluetun-server/main.sh
 pushd /tmp/qbittorrent-port-forward-gluetun-server > /dev/null || exit
 version=$(git rev-list --count --first-parent HEAD)
