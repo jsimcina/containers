820aa6242ace69913d7d35d3d330e0ebdc574420
Zilch25 <jsimcina@gmail.com>
2024-09-05 14:02:54 -0500

    rebuuuuuuuild

--- apps/audiobookshelf/ci/latest.sh
+++ apps/audiobookshelf/ci/latest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET "https://api.github.com/repos/advplyr/audiobookshelf/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
+tarball_url=$(curl -sX GET "https://api.github.com/repos/advplyr/audiobookshelf/releases/latest" | jq --raw-output '.tarball_url' 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
+printf "%s" "${tarball_url}"
