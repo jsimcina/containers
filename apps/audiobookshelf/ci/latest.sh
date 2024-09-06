9c82eefdbda2380bb1e0b702f1be6ea70398af30
Zilch25 <jsimcina@gmail.com>
2024-09-06 11:13:50 -0500

    okay i give up, just hardcode the download path >:(

--- apps/audiobookshelf/ci/latest.sh
+++ apps/audiobookshelf/ci/latest.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET "https://api.github.com/repos/advplyr/audiobookshelf/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-tarball_url=$(curl -sX GET "https://api.github.com/repos/advplyr/audiobookshelf/releases/latest" | jq --raw-output '.tarball_url' 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
-tarball_url="${tarball_url}"
 printf "%s" "${version}"
-printf "%s" "${tarball_url}"
